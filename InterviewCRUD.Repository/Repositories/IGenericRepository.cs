cfcb097b97816598559529641b474aaeb9c51ca3
leo <plus724254@gmail.com>
2021-08-03 21:59:25 +0800

    - 加入EF6套件，使用dbfirst - 加入DI套件 - 加入EF泛型存儲庫

---
+++ InterviewCRUD.Repository/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCRUD.Repository.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        T GetById(string id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void SaveChanges();
+    }
+}
---
+++ InterviewCRUD/Startup.cs
@@ -0,0 +1,48 @@
+﻿using InterviewCRUD.Repository.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+[assembly: OwinStartup(typeof(InterviewCRUD.Startup))]
+
+namespace InterviewCRUD
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            ConfigureServices(services);
+            var resolver = new DefaultDependencyResolver(services.BuildServiceProvider());
+            DependencyResolver.SetResolver(resolver);
+        }
+
+        public void ConfigureServices(Microsoft.Extensions.DependencyInjection.ServiceCollection services)
+        {
+            services.AddTransient(typeof(CourseSelectionEntities));
+        }
+    }
+
+    public class DefaultDependencyResolver : System.Web.Mvc.IDependencyResolver
+    {
+        protected IServiceProvider serviceProvider;
+        public DefaultDependencyResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public object GetService(Type serviceType)
+        {
+            return this.serviceProvider.GetService(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.serviceProvider.GetServices(serviceType);
+        }
+    }
+}
